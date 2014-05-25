0f621bf4bee6a950f4cf7ac0d296d2ef4b77f53f
annapham88 <annapham88@yahoo.com>
2014-05-25 16:13:14 -0700

    annapham88 program assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates a cached inverted square matrix that can be retrieved
+## outside of the environment in which it was created
+## the solve() function is used to compute the inverse of a squared matrix
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- NULL
+        
+        # set the value of the matrix
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        # get the value of the matrix
+        get <- function() x
+        
+        # set the value of the inverse
+        setInverse <- function(solve) m <<- solve
+        
+        # get the value of the inverse
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+        
 }
 
+#================================================================================
 
-## Write a short comment describing this function
-
+## This function returns a squared matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getInverse()
+        
+        # check to see if the matrix has already been created
+        # if already created, pulls the matrix from cached
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        # if matrix not created, retrieves the matrix and computes its inverse
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
+        
 }
+
